d13ba6137084e55f873f6afb67447fe8906cc951
Yuri Pieters <yuri@zopatista.com>
2023-03-05 17:35:58 +0000

    Use new addModule syntax

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "known-folders",
+    _ = b.addModule("known-folders", .{
         .source_file = .{ .path = "known-folders.zig" },
     });
 }
