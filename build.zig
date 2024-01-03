3f6a76a7cfd5b9e4c9b5890b1882380303ae888f
xdBronch <51252236+xdBronch@users.noreply.github.com>
2024-01-02 23:21:46 -0500

    Update build.zig

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("known-folders", .{
-        .source_file = .{ .path = "known-folders.zig" },
+        .root_source_file = .{ .path = "known-folders.zig" },
     });
 }
