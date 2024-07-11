13a7c0dac9ee454ac3574fb073b63beef0be2868
Techatrix <techatrix@mailbox.org>
2024-07-11 21:40:33 +0200

    build: add Zig version check

--- build.zig
+++ build.zig
@@ -1,6 +1,21 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+const minimum_zig_version = std.SemanticVersion.parse("0.12.0") catch unreachable;
 
 pub fn build(b: *std.Build) void {
+    if (comptime (builtin.zig_version.order(minimum_zig_version) == .lt)) {
+        @compileError(std.fmt.comptimePrint(
+            \\Your Zig version does not meet the minimum build requirement:
+            \\  required Zig version: {[minimum_zig_version]}
+            \\  actual   Zig version: {[current_version]}
+            \\
+        , .{
+            .current_version = builtin.zig_version,
+            .minimum_zig_version = minimum_zig_version,
+        }));
+    }
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
